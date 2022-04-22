e039895d91b1a2ae87516e1075432cb0eab58de8
he <slut@vip.qq.com>
2022-04-22 11:27:55 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/learning_c__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learning_c__.dir/main.cpp.o"
+  "learning_c__"
+  "learning_c__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learning_c__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/learning_c__.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for learning_c__.
+# This may be replaced when dependencies are built.
